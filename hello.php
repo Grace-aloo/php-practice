16234d07cc2148dd5356e644c9d4d4bdd0d6138e
Grace-aloo <graceaaloo@gmail.com>
2024-01-15 16:32:00 +0300

    working on php

--- hello.php
+++ hello.php
@@ -1,3 +1,14 @@
 <?php
-echo "hello world"
+echo "hello world";
+$cat = "mufasa";
+$n = 22;
+$n=$n*2;
+$n = $n/2;
+echo $cat;
+echo "My age is $n";
+//concatenation
+echo 'My age is ' . $n;
+$a = "n";
+//variable variable
+echo 'Number is ' . $$a;
 ?>
